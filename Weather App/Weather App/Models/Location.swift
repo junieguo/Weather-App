3e02146269d46c78ae3bb3cc02028b919db00e4a
junieguo <junieguo@sas.upenn.edu>
2025-04-10 23:30:50 -0400

    Update Location.swift

--- Weather App/Weather App/Models/Location.swift
+++ Weather App/Weather App/Models/Location.swift
@@ -3,3 +3,20 @@
 //  Weather App
 //
 
+struct Location: Identifiable, Decodable {
+    let lat: Double
+    let lon: Double
+    let name: String
+    let display_name: String
+    let address: Address
+
+    var id: String { "\(lat)_\(lon)" }
+}
+
+struct Address: Decodable {
+    let city: String?
+    let county: String?
+    let state: String
+    let country: String
+    let country_code: String
+}
